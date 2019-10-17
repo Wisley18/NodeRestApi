4c2ff44fcdea5d7d1f4e88c9ccb6505a24c21884
wisley.silva <wisley.silva@multilaser.com.br>
2019-10-17 19:15:04 -0300

    Implementação do login, gerando um otken criado com JWT.

--- api/routes/users.js
+++ api/routes/users.js
@@ -66,13 +66,20 @@ router.post('/login', (req, res, next) => {
                 }
 
                 if (result) {
-                    jwt.sign({
-                        email: user.email,
-                        userId: user._id
-                    }, process.env.JWT_TOKEN)
+                    const token = jwt.sign(
+                        {
+                            email: user.email,
+                            userId: user._id
+                        }, 
+                        process.env.JWT_KEY, 
+                        {
+                            expiresIn: '1h'
+                        }
+                    );
                     
                     return res.status(200).json({
-                        message: 'Auth successful'
+                        message: 'Auth successful',
+                        token: token
                     });
                 }
 
